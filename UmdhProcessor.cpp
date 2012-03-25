05bd726961c8b9571b2309c21744a308128e820f
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2012-03-25 11:23:08 +0900

    fix line-endings in umdh output (LF->CRLF)

--- UmdhProcessor.cpp
+++ UmdhProcessor.cpp
@@ -32,17 +32,17 @@ UmdhProcessor::UmdhProcessor(PCSTR filename)
 		}
 		throw -1;
 	}
-	CString str = "// Loaded modules:\n"
-		"//     Base Size Module\n";
+	CString str = "// Loaded modules:\r\n"
+		"//     Base Size Module\r\n";
 
 	std::vector<ModuleInfo> modules = GetLoadedModules();
 	for (std::vector<ModuleInfo>::iterator itr = modules.begin(); itr != modules.end(); itr++)
 	{
 		CString line;
-		line.Format("//    %16I64X %8I64X %s\n", itr->DllBase, itr->SizeOfImage, itr->FullDllName);
+		line.Format("//    %16I64X %8I64X %s\r\n", itr->DllBase, itr->SizeOfImage, itr->FullDllName);
 		str += line;
 	}
-	str += "//\n";
+	str += "//\r\n";
 	DWORD written;
 	if (!WriteFile(output_, (LPCSTR)str, str.GetLength(), &written, NULL) || written != (DWORD)str.GetLength())
 	{
@@ -68,14 +68,14 @@ void UmdhProcessor::StartHeap(ULONG64 heapAddress)
 		return;
 	}
 	CString str;
-	str.Format("\n"
-		"*- - - - - - - - - - Start of data for heap @ %I64X - - - - - - - - - -\n"
-		"\n"
-		"REQUESTED bytes + OVERHEAD at ADDRESS by BackTraceID\n"
-		"     STACK if not already dumped.\n"
-		"\n"
-		"*- - - - - - - - - - Heap %I64X Hogs - - - - - - - - - -\n"
-		"\n",
+	str.Format("\r\n"
+		"*- - - - - - - - - - Start of data for heap @ %I64X - - - - - - - - - -\r\n"
+		"\r\n"
+		"REQUESTED bytes + OVERHEAD at ADDRESS by BackTraceID\r\n"
+		"     STACK if not already dumped.\r\n"
+		"\r\n"
+		"*- - - - - - - - - - Heap %I64X Hogs - - - - - - - - - -\r\n"
+		"\r\n",
 		heapAddress, heapAddress);
 	
 	DWORD written;
@@ -94,9 +94,9 @@ void UmdhProcessor::FinishHeap(ULONG64 heapAddress)
 		return;
 	}
 	CString str;
-	str.Format("\n"
-		"*- - - - - - - - - - End of data for heap @ %I64X - - - - - - - - - -\n"
-		"\n", heapAddress);
+	str.Format("\r\n"
+		"*- - - - - - - - - - End of data for heap @ %I64X - - - - - - - - - -\r\n"
+		"\r\n", heapAddress);
 
 	DWORD written;
 	if (!WriteFile(output_, (LPCSTR)str, str.GetLength(), &written, NULL) || written != (DWORD)str.GetLength())
@@ -116,19 +116,19 @@ void UmdhProcessor::Register(ULONG64 ustAddress,
 
 	ULONG64 backtrace = ustAddress != 0 ? GetStackTraceArrayPtr(ustAddress) : 0;
 	CString str;
-	str.Format("%I64X bytes + %I64X at %I64X by BackTrace%I64X\n",
+	str.Format("%I64X bytes + %I64X at %I64X by BackTrace%I64X\r\n",
 		userSize, size - userSize, userAddress, backtrace);
 	if (ustAddress != 0 && processed_.find(backtrace) == processed_.end())
 	{
-		str = "\n" + str;
+		str = "\r\n" + str;
 		std::vector<ULONG64> trace = ::GetStackTrace(ustAddress);
 		for (std::vector<ULONG64>::iterator itr = trace.begin(); itr != trace.end(); itr++)
 		{
 			CString line;
-			line.Format("\t%I64X\n", *itr);
+			line.Format("\t%I64X\r\n", *itr);
 			str += line;
 		}
-		str += "\n";
+		str += "\r\n";
 		processed_.insert(backtrace);
 	}
 
