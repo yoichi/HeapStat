a1eee4f975432948e79ff93897cde5a47d0fe079
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2013-10-31 08:21:02 +0900

    a program for reaserch heap

---
+++ tools/heapstudy.cpp
@@ -0,0 +1,26 @@
+﻿/*
+	allocate various size of memories and fill them
+*/
+#include <intrin.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	size_t blockSize[] = {256, 512, 1024, 2048, 4096, 8192, 16384};
+	for (int i = 0; i < _countof(blockSize); i++)
+	{
+		for (int j = 0; j < 0x100; j++)
+		{
+			size_t length = blockSize[i] - 0x20 - (j >> 4);
+			void *ptr = malloc(length);
+			memset(ptr, 0xc0 | i, length);
+			printf("ptr=%p,i=0x%02x\n", ptr, i);
+		}
+	}
+
+	__debugbreak();
+
+	return 0;
+}
